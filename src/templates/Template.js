989623cc809441dcff5d5eca307ef1a9d0e11be2
Ricardo Perez <ricardo.pereze.98@gmail.com>
2022-03-02 10:02:41 -0400

    worked with html css and js with webpack

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,4 +1,7 @@
 import getData from '../utils/getData.js';
+import github from '../assets/images/github.png'
+import twitter from '../assets/images/twitter.png'
+import instagram from '../assets/images/instagram.png'
 
 const Template = async () => {
   const data = await getData();
@@ -23,14 +26,14 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
-            <img src="../src/assets/images/twitter.png" />
+          <a href="xD">
+            <img src="${twitter}" />
           </a>
           <a href="https://github.com/gndx">
-            <img src="../src/assets/images/github.png" />
+            <img src="${github}" />
           </a>
           <a href="https://instagram.com/gndx">
-            <img src="../src/assets/images/instagram.png" />
+            <img src="${instagram}" />
           </a>
         </div>
       </div>
